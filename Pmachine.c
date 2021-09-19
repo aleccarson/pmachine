73a553545062efea5fe167bf9df9a984421ee9ab
Alec <aleccarsonn@gmail.com>
2021-09-19 12:30:37 -0400

    first commit

---
+++ Pmachine.c
@@ -0,0 +1,17 @@
+// Alec Carson Fall 2021
+// COP3402 HW 1
+#include<stdio.h>
+// IR struct (obviously)
+struct IR {
+    int OP;
+    int L;
+    int M;
+};
+
+int main (int argc, char *argv[])   {
+    // create and set PAS array to 0
+    int PAS[500] = {0};
+    struct IR pog[150];
+    
+    return 0;
+}
